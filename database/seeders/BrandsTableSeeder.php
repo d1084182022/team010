ee85508d18f716787a30321d146654c9ce4bfdba
DESKTOP-UITHU5S\F309 <D1084182022@gm.lhu.edu.tw>
2021-11-24 17:36:05 +0800

    利用seeders隨機生成資料

--- database/seeders/BrandsTableSeeder.php
+++ database/seeders/BrandsTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class BrandsTableSeeder extends Seeder
 {
@@ -11,8 +13,44 @@ class BrandsTableSeeder extends Seeder
      *
      * @return void
      */
+
+
+    public function generateRandomBrands() {
+        $brands = ['Aprilia',
+                   'BMW',
+                   'Ducati',
+                   'Honda',
+                   'KTM',
+                   'Kawasaki',
+                   'Suzuki',
+                   'Yamaha'
+];
+        return $brands[rand(0, count($brands)-1)];
+    }
+    public function generateRandomCountry() {
+        $countries = [
+            '日本',
+            '義大利',
+            '德國',
+            '奧地利'
+        ];
+        return $countries[rand(0, count($countries)-1)];
+    }
     public function run()
     {
         //
+        for ($i=0; $i<9; $i++) {
+            $countries = $this->generateRandomCountry();
+            $brands=$this->generateRandomBrands();
+
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+
+            DB::table('teams')->insert([
+                'name'=>$brands,
+                'country' => $countries,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
     }
 }
--- database/seeders/EngineTypesTableSeeder.php
+++ database/seeders/EngineTypesTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class EngineTypesTableSeeder extends Seeder
 {
@@ -11,8 +13,30 @@ class EngineTypesTableSeeder extends Seeder
      *
      * @return void
      */
+    public function generateRandomEngineTypes()
+    {
+        $enginetypes = [
+            '水冷四行程DOHC並列雙缸引擎',
+            '水冷4缸DOHC 16V',
+            '空油冷2缸DOHC 8V',
+            '水冷2缸DOHC 8V',
+            '機械增壓+水冷4缸DOHC 16V',
+            '水冷3缸DOHC 12V'
+        ];
+        return $enginetypes[rand(0, count($enginetypes) - 1)];
+    }
     public function run()
     {
         //
+        for ($i=0; $i<7; $i++) {
+            $enginetypes = $this->generateRandomEngineTypes();
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+
+            DB::table('teams')->insert([
+                'engine_type'=>$enginetypes,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+        }
     }
 }
