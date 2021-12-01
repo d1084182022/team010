41ccf991e079786c622095592ebdbc7a7eb9866a
DESKTOP-UITHU5S\F309 <D1084182022@gm.lhu.edu.tw>
2021-12-01 11:54:21 +0800

    建立controller與model 測試&讀取網頁成功

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\MotorcyclesController;
+use App\Http\Controllers\BrandsController;
+use App\Http\Controllers\EngineTypesController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-
-    return "hello world";
+    return "Hello World";
 });
+route::resource("motorcycles", MotorcyclesController::class)  ;
+route::resource("brands", BrandsController::class)  ;
+route::resource("enginetypes", EngineTypesController::class)  ;
