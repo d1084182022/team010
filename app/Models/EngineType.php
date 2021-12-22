31f9a367f3b7f37eb22b3012efea84290b6326f2
DESKTOP-UITHU5S\F309 <D1084182022@gm.lhu.edu.tw>
2021-12-22 10:06:03 +0800

    增設引擎形式的新增、修改、刪除

--- app/Models/EngineType.php
+++ app/Models/EngineType.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class EngineType extends Model
 {
     protected $fillable=[
-        'name'
+        'engine_type'
     ];
     use HasFactory;
 }
--- resources/views/enginetypes/index.blade.php
+++ resources/views/enginetypes/index.blade.php
@@ -5,11 +5,15 @@
 </head>
 <body>
 <h1>顯示所有引擎資料</h1>
+<a href="enginetypes/create">
+    新增<br>
+</a>
 <table border="1">
     <tr>
         <th>編號</th>
         <th>引擎形式</th>
         <th>修改</th>
+        <th>刪除</th>
 
     </tr>
 
@@ -21,6 +25,13 @@
                 <a href="enginetypes/{{$engine_type->id}}/edit">
                     修改
                 </a>
+            </td>
+            <td>
+                <form method="post" action="enginetypes/{{$engine_type->id}}">
+                    @csrf
+                    @method("delete")
+                    <input type="submit" value="刪除"/>
+                </form>
             </td>
         </tr>
     @endforeach
