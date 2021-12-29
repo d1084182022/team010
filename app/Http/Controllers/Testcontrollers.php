c271b0540566b67cf681f8546ed9d7c148385f76
DESKTOP-UITHU5S\F309 <D1084182022@gm.lhu.edu.tw>
2021-12-29 16:52:33 +0800

    增加 api 的註冊與登入功能

---
+++ app/Http/Controllers/Testcontrollers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Testcontrollers extends Controller
+{
+    //
+    public function helloworld(){
+        return 'hello';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Testcontrollers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', [AuthController::class, 'register']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('login',  [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {});
